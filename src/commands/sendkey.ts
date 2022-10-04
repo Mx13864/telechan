db8c7b5fb8bbd6feb61796335997194c1740167e
Mx13864 <82318029+Mx13864@users.noreply.github.com>
2022-10-05 00:43:10 +0800

    Update sendkey.ts

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -15,11 +15,15 @@ const sendkey = () => (ctx: any) => {
 
   const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
   sum++;
-  if(sum=1){
+  if(sum==0){
   replyToMessage(ctx, messageId, `${userName} , Your sendkey is 🔑 ${sendkey} \n 
   🚀 Use follow url to send message : \n 
   ${site_url}/api/send?sendkey=<sendkey>&text=<text>`);
   // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
+  }else if(sum!=0){
+    replyToMessage(ctx, messageId, `${userName} , Your sendkey is 🔑 ${sendkey} \n 
+  🚀 Use follow url to send message : \n 
+  ${site_url}/api/send?sendkey=<sendkey>&text=<text>`);
   }
 };
 
